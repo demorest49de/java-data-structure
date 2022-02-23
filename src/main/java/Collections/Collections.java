8341ee9bdbb53a9ff4fd4715342a5fdd7c668875
shevchenkoak <demorest49de@gmail.com>
2022-02-23 23:58:30 +0300

    init

--- src/main/java/Collections/Collections.java
+++ src/main/java/Collections/Collections.java
@@ -4,8 +4,21 @@ import java.util.*;
 
 public class Collections {
     public static void main(String[] args) {
-        List<String> arrayList = new ArrayList<>();
-        List<String> linkedList = new LinkedList<>();
+        List<Integer> list = new LinkedList<>();
+        List<Integer> arraylist = new ArrayList<>();
+
+        for(Integer i = 0; i < 10; i++){
+            list.add(i);
+            System.out.println(i);
+        }
+
+        list.remove(6);
+        System.out.println(list);
+
+        list = new LinkedList<Integer>();
+
+
+
 
 
         Set<String> hashSet = new HashSet<>();
@@ -18,7 +31,7 @@ public class Collections {
         LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
 
         WeakHashMap<String, String> weakHashMap = new WeakHashMap<>();
-        TreeMap<String, String> treeMap = new TreeMap<>();
 
+        TreeMap<String, String> treeMap = new TreeMap<>();
     }
 }
