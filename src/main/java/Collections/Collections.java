c274ae59812829125b5f976e578edc590989db55
shevchenkoak <demorest49de@gmail.com>
2022-02-25 21:31:16 +0300

    init

--- src/main/java/Collections/Collections.java
+++ src/main/java/Collections/Collections.java
@@ -9,11 +9,9 @@ public class Collections {
 
         for(Integer i = 0; i < 10; i++){
             list.add(i);
-            System.out.println(i);
         }
 
         list.remove(6);
-        System.out.println(list);
 
         list = new LinkedList<Integer>();
 
@@ -28,10 +26,54 @@ public class Collections {
         TreeSet<String> treeSet = new TreeSet<>();
 
         HashMap<String, String> hashMap = new HashMap<>();
-        LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
+        LinkedHashMap <String, String> linkedHashMap = new LinkedHashMap<>();
 
         WeakHashMap<String, String> weakHashMap = new WeakHashMap<>();
 
         TreeMap<String, String> treeMap = new TreeMap<>();
+
+        Set<Integer> integers = new HashSet<>();
+
+        List<Object> objects = new ArrayList<>();
+        objects.add("1vasja");
+        objects.add(1);
+        objects.add(1l);
+        objects.add(1.0);
+
+        System.out.println(objects);
+
+        TreeSet ts = new TreeSet();
+
+        // Добавляем элементы к набору древ
+        ts.add("C");
+        ts.add("A");
+        ts.add("B");
+        ts.add("E");
+        ts.add("F");
+        ts.add("D");
+        ts.add("D");
+        System.out.println(ts);
+
+        Map m2 = new HashMap();
+
+        m2.put("Маша", "8");
+        m2.put("Михаил", "31");
+        m2.put("Олег", "12");
+        m2.put("Денис", "14");
+
+        System.out.println();
+        System.out.println("Элементы карты");
+        System.out.print("\t" + m2);
+
+        var m1 = new TreeMap();
+
+        m1.put("Маша", "8");
+        m1.put("Михаил", "31");
+        m1.put("Олег", "12");
+        m1.put("Денис", "14");
+
+        System.out.println();
+        System.out.println("Элементы карты");
+        System.out.print("\t" + m1);
     }
 }
---
+++ src/main/java/Collections/Task_About_Collections.java
@@ -0,0 +1,16 @@
+package Collections;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class Task_About_Collections {
+    public static void main(String[] args) {
+//        добавить в список уник значений коллекцию  с неуникальнымии значенями.
+        //() -> список - Интежер
+        // отстортировать по возрастанию
+        // и вывести
+        // Collections class = много доп методов и там есть сортировка.
+
+
+    }
+}
